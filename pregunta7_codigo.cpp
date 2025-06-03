354e41c9b1ff1ec4e150c6d758cf500f16d57165
Diego Neira <diegoneira@gmail.com>
2025-06-02 22:26:07 -0400

    problema 7

--- pregunta7_codigo.cpp
+++ pregunta7_codigo.cpp
@@ -11,10 +11,13 @@
  *  - lineas adicionales: correcciones de chat-GPT o de fuentes externas
  *  - bloque comentado después del código: análisis asintótico
 */
+
 #include <iostream>
-#include <fstream>
+#include <string>
+#include <list>          // lista doblemente enlazada
 
 using namespace std;
+
 // ############## ANÁLISIS DEL PROBLEMA ##############
 /*
 * Descripción del problema:
@@ -33,22 +36,63 @@ using namespace std;
 * Aproximación:
 * se tomaran las lineas y se procesarán hasta encontrar uno de los 
 * caracteres y entonces se cambiará el modo de incertar de al final
-* del arreglo al principio o vice-versa 
+* del arreglo al principio o vice-versa, para esto voy a usar 
+* una lista doblemente enlazada para poder agregar al comienzo o 
+* final segun corresponda
 */
 
 
 // ############## CODIGO ##############
-int main (int argc, char *argv[]) {
-  return 0;
+
+
+int main() {
+    string linea;// definimos la variable donde almacenaremos el texto de la linea
+    
+    // Leemos cada línea hasta fin de archivo (EOF), cuando llegamos al final del archivo
+    // y no hay mas lineas por leer getline arroga False asi que escapamos correctamente
+    while (getline(cin, linea)) {
+        list<char> resultado;             // Lista doble enlazada para almacenar el texto final
+        auto cursor = resultado.begin();  // Iterador que simula la posición del cursor
+
+        for (char c : linea) {// recorremos cada caracter en la linea uno por uno
+            if (c == '[') { // cuando encontramos el signo de home
+                // Simula tecla "inicio" entonces movemos el cursor al inicio
+                cursor = resultado.begin();
+            } else if (c == ']') {
+                // Simula tecla "fin" asi que muevo el cursor al final
+                cursor = resultado.end();
+            } else {
+                // cuando encontramos cualquier otro caracter hacemos la insecion diracta
+                // en la posicion en que el cursor nos tiene posicionados
+                resultado.insert(cursor, c);
+            }
+        }
+
+        // Imprimimos el resultado final haciendo un cout de cada caracter en la lista ya
+        // en el orden que debe imprimirse
+        for (char c : resultado) {
+            cout << c;
+        }
+        // terminamos con el salto de linea par terminar la linea
+        cout << endl;
+    }
+
+    return 0;
 }
-
-
 
 
 // ############## ANÁLISIS ASINTÓTICO ##############
 /*
 * tiempo:
+* por cada linea el ciclo for se ejecuta solo una vez
+* por cada caracter en la linea por lo que es tiempo lineal
+* todas las demas operaciones son de tiempo constante, por 
+* lo tanto, si consideramos n los caracteres de todas las lineas:
+* O(n)
 *
 * espacio:
+* la estructura principal en la que se almacenan todos los n 
+* caracteres por lo que la complejidad espacial es:
+* O(n)
 *
 */
